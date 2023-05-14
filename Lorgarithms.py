c6e9a5110bc6ced96d3ba9b041798665bdd3d0b4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-14 22:38:47 +0300

    Update and rename Exponents_Part_2.py to Lorgarithms.py

---
+++ Lorgarithms.py
@@ -0,0 +1,358 @@
+A logarithm is the power to which a number must be raised in order to get some other number (see Section 3 of this Math Review for more about exponents). For example, the base ten logarithm of 100 is 2, because ten raised to the power of two is 100:
+
+log 100 = 2
+
+because
+
+102 = 100
+
+This is an example of a base-ten logarithm. We call it a base ten logarithm because ten is the number that is raised to a power. The base unit is the number being raised to a power. There are logarithms using different base units. If you wanted, you could use two as a base unit. For instance, the base two logarithm of eight is three, because two raised to the power of three equals eight: 
+
+log2 8 = 3
+
+because
+
+23 = 8
+
+In general, you write log followed by the base number as a subscript. The most common logarithms are base 10 logarithms and natural logarithms; they have special notations. A base ten log is written 
+
+log
+
+and a base ten logarithmic equation is usually written in the form:
+
+log a = r 
+
+A natural logarithm is written
+
+ln
+
+and a natural logarithmic equation is usually written in the form:
+
+ln a = r
+
+So, when you see log by itself, it means base ten log. When you see ln,
+it means natural logarithm (we'll define natural logarithms below). In this course only base ten and natural logarithms will be used.
+                            
+                            
+                            PAGE 2
+               ***************************************************             
+                            Base Ten Logarithms
+We saw above that base ten logarithms are expressions in which the number being raised to a power is ten. The base ten log of 1000 is three:
+
+log 1000 = 3
+
+103 = 1000
+
+So far, we've worked with expressions that have whole numbers as solutions. Here's one that does not. What is the log of 4?
+
+log 4 = x
+
+log 4 ≈ 0.602
+
+because
+
+10 0.602 ≈ 4
+
+ 
+
+Natural Logarithms
+Logarithms with a base of 'e' are called natural logarithms. What is 'e'?
+
+'e' is a very special number approximately equal to 2.718. 'e' is a little bit like pi in that it is the result of an equation and it's a big long number that never ends. For those of you who have had calculus, you might remember that ex is special because its derivative is itself. If you want to know more about 'e', check any trigonometry text, such as page 234 of Ruud, W.L. and T.L. Shell. Prelude to Calculus, 2nd ed. 1993. Boston: PWS Publishing Company.
+
+Most scientific calculators have an 'e' button and an 'ln' button, so you don't need to memorize the value of 'e'.
+                            
+                            
+                            
+                            
+            PAGE 3
+                            Logarithmic Rules
+Just as exponents have some basic rules that make them easier to manipulate (see Section 3: Exponents), so do logarithms. These rules apply to all logarithms, including base 10 logarithms and natural logarithms. For simplicity's sake, base ten logs are used in most of these rules:
+
+1.   br = a is the equivalent to logb a=r (This is the definition of a logarithm.)
+
+2.   log 0 is undefined.
+
+3.   log 1 = 0
+
+4.   log (P*Q) = log P + log Q
+
+5.   log (P/Q) = log P - log Q
+
+6.   log (Pt) = t *log P
+
+7.   10(log a) = a (in the case of natural logarithms, e(ln a) = a)
+
+8.   log (10r) = r (in the case of natural logarithms, ln er = r)
+
+9.   log (1/a) = -log a
+
+Let's take a closer look at each of these rules:
+
+ 
+
+1. br = a is the equivalent of logb a=r. We've already looked at how this works, but here's another example:
+
+log 14 ≈ 1.146
+
+is the equivalent of
+
+10 1.146 ≈ 14
+
+ 
+
+2. log 0 is undefined. It's not a real number, because you can never get zero by raising anything to the power of anything else. You can never reach zero, you can only approach it using an infinitely large and negative power.
+
+ 
+
+3. log 1 = 0 means that the logarithm of 1 is always zero, no matter what the base of the logarithm is. This is because any number raised to 0 equals 1. Therefore, ln 1 = 0 also.
+
+All the rest of the logarithmic rules are useful for solving complex equations, or equations with unknowns.
+                            
+                            PAGE 4
+                            
+                            
+                            
+       4. log (P*Q) = log P + log Q means that if you take the logarithm of two factors, it is the same as taking the logarithm of each factor, and adding them together. For example:
+
+log 6 =
+
+log (2 * 3) =
+
+log 2 + log 3 ≈
+
+0.301 + 0.477 = 0.778
+
+If you were using natural logarithms, it would look like this:
+
+ln 6 =
+
+ln (2 * 3) =
+
+ln 2 + ln 3 ≈
+
+0.693 + 1.099 = 1.792
+
+(Note that the numerical value of the natural logarithm is different from that of the base ten logarithm. That's because, in the second example, 1.792 is the power to which 'e' must be raised to get 6, whereas, in the first example, 0.778 is the power to which 10 must be raised to get 6.)
+
+If you have a variable as one of your factors, it would look like this:
+
+log 2y = log 2 + log y
+
+Let's say log 2y = 36 and solve for y:
+
+log 2y = 36
+
+log 2 + log y = 36
+
+log y = 36 - log 2
+
+log y = 36 - 0.301
+
+log y = 35.699
+
+y = 10 35.699
+
+which is a really big number.
+
+ 
+
+5. log (P/Q) = log P - log Q means that if you take a logarithm of one number divided by another, it is the same as taking each logarithm separately, and then subtracting the logarithm of the denominator from the logarithm of the numerator.
+
+For example:
+
+log (3 / 2) =
+
+log 3 - log 2 ≈
+
+0.477 - 0.301 ≈ 0.176
+
+If you were using natural logarithms, it would look like this:
+
+ln (3 / 2) =
+
+ln 3 - ln 2 ≈
+
+1.099 - 0.693 ≈ 0.406
+
+If you have a variable as one of your factors, it would look like this:
+
+log (y/2) = log y - log 2
+
+Let's say log (y/2) = 36 and solve for y:
+
+log (y/2) = 36
+
+log y - log 2 = 36
+
+log y = 36 + log 2
+
+log y = 36 + 0.301
+
+log y = 36.301
+
+y = 10 36.301
+
+which is an even bigger number.
+
+ 
+
+6. log (Pt) = t * log P means that the logarithm of a number raised to some power, it is the same as multiplying the logarithm of that number by the value of the power. 
+
+For example:
+
+log (32) = 2 * log 3 
+
+2 * 0.477 = 0.954
+
+It looks the same when you use natural logarithms, however, as in example three the numerical value will be different.
+
+ln (32) = 2 * ln 3
+
+2 * 1.099 = 2.198                     
+                            
+                            
+                            
+          PAGE 5
+ 
+ 7. 10(log a) = a (or, in the case of natural logarithms, e(ln a) = a). Logarithms and exponents reverse each other.
+
+For example:
+
+10(log 3) = 3
+
+10(log 8) = 8
+
+e(ln 3) = 3
+
+e(ln 8) = 8
+
+If you raise a number to the power of a logarithm that has that number as its base, it is equal to the number that you used in the logarithm.
+
+8. log (10r) = r (in the case of natural logarithms, ln er = r)Because logarithms and exponents reverse each other, this rule is similar to rule number seven.
+
+For example:
+
+log (102) = 2
+
+log (103) = 3
+
+ln (e2) = 2
+
+ln (e4) = 4
+
+Any logarithm of its base number raised to some exponent is equal to that exponent.
+
+ 
+
+9. log (1/a) = -log a means that the logarithm of 1 divided by some number is equal to the negative logarithm of that number. (This is the exactly the opposite of the rule governing exponents where a number raised to a negative number is equal to 1 divided by that number raised to that power.)
+
+For example:
+
+log (1/2) = - log 2 = -0.301 
+
+log (1/3) = - log 3 = -0.477 
+
+ln (1/2) = -ln 2 = -0.693
+
+ln (1/3) = -ln 3 = -1.099
+
+
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
